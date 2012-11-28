c41cdb3f2a3d98819b65423a445a28b779e6fc08
RefaktorCo <refaktor.co@gmail.com>
2012-11-28 12:23:27 -0800

    -Basic isotope functionality, going to try and work around the image load JS hack.

--- includes/front-page.php
+++ includes/front-page.php
@@ -136,6 +136,17 @@ function blocks_front_page($page){
 
    <div class="row" >
      <div style="margin-top: 150px;"></div>
+   </div> 
+   
+  
+   
+   <div class="row">
+       <div class="twelve columns">
+         <h3>Live Tiles Playground</h3>
+       </div>
+     </div> 
+       
+     <div class="row">   
        <div id="about" class="six columns">
          <div class="row">      
        
@@ -207,6 +218,11 @@ function blocks_front_page($page){
 			
       </div>
   
+     <div class="row">
+       <div class="twelve columns">
+         <h3>Carousel Playground</h3>
+       </div>
+     </div> 
     
      <div class="row">
       <div class="twelve columns">
@@ -383,15 +399,237 @@ function blocks_front_page($page){
         </ul>
       
         
-             </div>
-             
-      </div>
+       </div>      
       </div>
     </div>
-
+    
+  <div class="row">
+       <div class="twelve columns">
+         <h3>Isotope Playground</h3>
+       </div>
+     </div>   
+    
+  <div class="row">
+    <div class="twelve columns">
+    <div class="row">    
+     <section id="options" class="clearfix">
+	      <ul id="filters" class="option-set clearfix" data-option-key="filter">
+	        <li><a href="#filter" data-option-value="*" class="selected small button">All</a></li>
+	        <li><a href="#filter" data-option-value=".cat_1" class="small button">Cat. 1</a></li>
+	        <li><a href="#filter" data-option-value=".cat_2" class="small button">Cat. 2</a></li>
+	        <li><a href="#filter" data-option-value=".cat_3" class="small button">Cat. 3</a></li>
+	      </ul>
+      </section>
+    
+      <div id="isotope_test">
+    
        
-  
-   
+    <div class="two columns switch cat_1" data-category="cat_1">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+          
+        </div>
+        <div class="portfolio_text">
+        Portfolio 1
+        </div>
+      </div>  
+    </div>
+    
+    <div class="two columns switch cat_1" data-category="cat_1 cat_2">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 2
+        </div>
+      </div>  
+    </div>
+    
+    <div class="two columns switch cat_2" data-category="cat_2">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 3
+        </div>
+      </div>  
+    </div>
+    
+    <div class="two columns switch cat_3" data-category="cat_3 cat_2">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 4
+        </div>
+      </div>  
+    </div>
+    
+    <div class="two columns switch cat_1" data-category="cat_1 cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 5
+        </div>
+      </div>  
+    </div>
+    
+    <div class="two columns switch cat_1" data-category="cat_1">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 6
+        </div>
+      </div>  
+    </div>
+    
+    <div class="two columns switch cat_2" data-category="cat_2 cat_1">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 7
+        </div>
+      </div>  
+    </div>
+    
+    <div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+    
+    <div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+<div class="two columns switch cat_3" data-category="cat_3">
+      <div class="portfolio_item">
+        <div class="portfolio_image">
+          <img src="<?php echo $root;?>/images/portfolio_4.jpg">
+        </div>
+        <div class="portfolio_text">
+        Portfolio 8
+        </div>
+      </div>  
+    </div>
+
+      </div>
+       </div>
+      </div>
+
+    </div>
+  </div>  
+  </div>
   <!-- end main wrap -->     
 
   
--- includes/header.php
+++ includes/header.php
@@ -46,6 +46,51 @@ function blocks_header($page){
 <script type="text/javascript">
   jQuery(document).ready(function ($) {
   
+   var $container = $('#isotope_test');
+
+      $container.isotope({
+        itemSelector : '.switch'
+      });
+      
+      $container.imagesLoaded( function(){
+        $container.isotope({
+			    // options...
+			  });
+			});
+      
+      
+      var $optionSets = $('#options .option-set'),
+          $optionLinks = $optionSets.find('a');
+
+      $optionLinks.click(function(){
+        var $this = $(this);
+        // don't proceed if already selected
+        if ( $this.hasClass('selected') ) {
+          return false;
+        }
+        var $optionSet = $this.parents('.option-set');
+        $optionSet.find('.selected').removeClass('selected');
+        $this.addClass('selected');
+  
+        // make option object dynamically, i.e. { filter: '.my-filter-class' }
+        var options = {},
+            key = $optionSet.attr('data-option-key'),
+            value = $this.attr('data-option-value');
+        // parse 'false' as false boolean
+        value = value === 'false' ? false : value;
+        options[ key ] = value;
+        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+          // changes in layout modes need extra logic
+          changeLayoutMode( $this, options )
+        } else {
+          // otherwise, apply new options
+          $container.isotope( options );
+        }
+        
+        return false;
+      });
+
+  
   $('#recent_projects').carouFredSel({
     width: '100%',
     responsive: true,
