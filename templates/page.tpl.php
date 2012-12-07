6ecabd38a28812eb35edf192d886ab435419959b
refaktorco <refaktor.co@gmail.com>
2012-12-07 09:45:32 -0800

    -More style stuff related to content

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -2,14 +2,17 @@
   
   
          
-          <div class="row">
-            <div class="twelve columns">
-              <div id="page_heading">
-              <h2 class="page_heading_text"><?php print $title; ?></h2>
-              <div id="breadcrumbs"><h3><?php if (theme_get_setting('breadcrumbs') == '1') {print $breadcrumb . $title; } ?></h3></div>
-              </div>
-            </div>
-          </div> 
+      <div id="heading_wrapper">
+    <div id="heading_wrapper_after">
+   	  <div class="row">
+        <div class="twelve columns">
+          <h2 class="page_heading_text"><?php print $title; ?></h2>
+          <div id="breadcrumbs"><h3><?php if (theme_get_setting('breadcrumbs') == '1') {print $breadcrumb . $title; } ?></h3></div>
+        </div>
+      </div>
+    </div> 
+  </div>
+ 
           
           <div class="row">
             <div id ="main_content_wrap" class="<?php if (theme_get_setting('page_layout') == 'sidebar_right') { echo "eight columns"; } else { echo "twelve columns"; } ?>">
