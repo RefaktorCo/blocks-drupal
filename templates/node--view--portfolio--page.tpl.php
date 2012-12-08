611e658b8375dbf5b3475f3e9bd5b6c3b800bb49
refaktorco <refaktor.co@gmail.com>
2012-12-07 16:29:10 -0800

    -Quick str_replace on tags to add / instead of spaces for display on hover

--- templates/node--view--portfolio--page.tpl.php
+++ templates/node--view--portfolio--page.tpl.php
@@ -1,6 +1,7 @@
 <?php global $root, $base_url;
 
-
+$tags = render($content['field_portfolio_tags']);
+$tags = str_replace(' ', '/',$tags);
 
 ?>
 
@@ -17,7 +18,7 @@
 	           </div>
       
       <div class="carousel_item_hover">
-	      <p><?php print $title; ?> </p>
+	      <p><?php echo $tags;?></p>
 	    
 	      <p><a style="line-height: 12px;" href="<?php echo file_create_url($node->field_portfolio_image['und'][0]['uri']); ?>" rel="lightbox" title="<?php print $title; ?>"><i class="general foundicon-search"></i></a><a href="<?php print $node_url;?>" style="line-height: 12px;" > <i class="general foundicon-paper-clip"></i></a></p>
 	    </div>
