3f82cecbd5478ff9ae9cba10d79773711ebdffd2
refaktorco <refaktor.co@gmail.com>
2012-12-12 11:46:41 -0800

    -Style switcher all separated from rest of template files.

--- includes/init.php
+++ includes/init.php
@@ -3,6 +3,8 @@
 require_once(drupal_get_path('theme', 'blocks').'/includes/header.php');
 require_once(drupal_get_path('theme', 'blocks').'/includes/front-page.php');
 require_once(drupal_get_path('theme', 'blocks').'/includes/footer.php');
+
+require_once(drupal_get_path('theme', 'blocks').'/includes/switch.php');
 	
 ?>
 
