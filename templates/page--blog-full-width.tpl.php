45b494e547eb3f6291be9f6e20dd50643ae38b35
refaktorco <refaktor.co@gmail.com>
2013-01-02 10:10:43 -0800

    -Changelog tweak

--- templates/page--blog-full-width.tpl.php
+++ templates/page--blog-full-width.tpl.php
@@ -12,17 +12,16 @@
     </div> 
   </div>
           
-          <div class="row">
-            <div id ="main_content_wrap" class="twelve columns">
-              <div id="main_content">
-	              <?php print render($title_prefix); ?>
-	              <?php print render($title_suffix); ?>
-	              <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-	              <?php print render($page['content']); ?>
-	            </div>
-	          </div>
-        
-		     </div>
+  <div class="row">
+    <div id ="main_content_wrap" class="twelve columns">
+      <div id="main_content">
+        <?php print render($title_prefix); ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php print render($page['content']); ?>
+      </div>
+    </div>
+ </div>
 
   
 <?php blocks_footer($page);?>
