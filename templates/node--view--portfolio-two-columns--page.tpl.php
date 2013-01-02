c8f5e31f34f9676d5c0f2f271acf09776f8dcad8
refaktorco <refaktor.co@gmail.com>
2013-01-02 09:22:24 -0800

    -Custom two column template for demo, just need to copy for three and change the value and make the view

---
+++ templates/node--view--portfolio-two-columns--page.tpl.php
@@ -0,0 +1,22 @@
+<?php 
+  global $root, $base_url; 
+  $tags = render($content['field_portfolio_tags']);
+  $tags = str_replace(' ', '/',$tags);
+?>
+
+  <div class="six columns switch <?php print render($content['field_portfolio_tags']); ?>" data-category="<?php print render($content['field_portfolio_tags']); ?>">
+    <div class="carousel_item_wrapper"> 
+      <div class="carousel_item_content">
+	      <div class="carousel_item_image">
+	        <?php print render($content['field_portfolio_image']); ?>
+	      </div>
+	    </div>
+      <div class="carousel_item_hover">
+	      <p><?php echo $tags;?></p>
+	      <p><a href="<?php echo file_create_url($node->field_portfolio_image['und'][0]['uri']); ?>" rel="lightbox" title="<?php print $title; ?>"><i class="general foundicon-search"></i></a><a href="<?php print $node_url;?>" > <i class="general foundicon-paper-clip"></i></a></p>
+	    </div>
+    </div>  
+    <div class="carousel_item_description">
+      <h3><a href="<?php print $node_url;?>"><?php print $title; ?></a></h3>
+    </div>
+  </div>
