99524040d0a8521b304aa1fab346b253a46462d8
refaktorco <refaktor.co@gmail.com>
2013-01-09 11:37:15 -0800

    -Removed contact template block region (no longer needed)

--- includes/footer.php
+++ includes/footer.php
@@ -11,52 +11,26 @@ function blocks_footer($page){
         
           <div class="three columns">
             <?php if(!$page['footer_1']) {?>
-            <h2>About Us</h2>
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.</p>
-            <ul class="footer_icons">
-           
-              <li><a href="#" target="_blank"><i class="social foundicon-twitter"></i></a></li>
-              <li><a href="#" target="_blank"><i class="social foundicon-facebook"></i></a></li>
-              <li><a href="#" target="_blank"><i class="social foundicon-linkedin"></i></a></li>
-              <li><a href="#" target="_blank"><i class="social foundicon-pinterest"></i></a></li>
-              <li><a href="#" target="_blank"><i class="social foundicon-google-plus"></i></a></li>
-              <li><a href="#" target="_blank"><i class="social foundicon-flickr"></i></a></li>
-                     
-            </ul> 
+            <h2>Footer Block 1</h2>
             <?php } else { print render($page['footer_1']); }?>   
           </div> 
       
           <div class="three columns">
             <?php if(!$page['footer_2']) {?>
-            <h2>Footer 2</h2>
+            <h2>Footer Block 2</h2>
             <?php } else { print render($page['footer_2']); }?>  
           </div>
           
           <div class="three columns">
             <?php if(!$page['footer_3']) {?>
-            <h2>Footer 3</h2>
+            <h2>Footer Block 3</h2>
             <?php } else { print render($page['footer_3']); }?>  
           </div>
           
           <div class="three columns">
             <?php if(!$page['footer_4']) {?>
-            <h2>Latest Tweets</h2>
-            <div class='tweet query'></div>
-							<script type="text/javascript">
-							                        
-							    jQuery(document).ready(function ($) {
-							      
-							      $(".tweet").tweet({
-							        username: "envato",
-							        avatar_size: 34,
-							        count: 3,
-							        loading_text: "loading tweets..."
-							      });
-							    
-							    });
-							    
-							  </script>
-          <?php } else { print render($page['footer_4']); }?>      
+            <h2>Footer Block 4</h2>
+            <?php } else { print render($page['footer_4']); }?>      
           </div>
           
         </div> 
--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -52,97 +52,8 @@
     <div class="row">   
       <div class="twelve columns">
       <?php if(!$page['services']) {?>
-     
-        <div class="row">
-		      <div class="six columns">
-		        <div class="row">      
-		       
-						   <div class="six columns services">  
-						     <div class="services_content">  
-						       <div class="team_image"> 
-						         <img src="<?php echo $root;?>/images/team3.jpg" alt="team"> 
-						       </div>
-						       <div class="team_image_hover">
-						         <h2>Steve MqQueen - CEO</h2>
-						         <p><a href="#" data-reveal-id="team1_modal"><i class="general foundicon-search"></i></a></p>
-						       </div>
-						     </div>        
-						   </div>
-						    
-						   <div class="six columns services">  
-						     <div class="services_content">   
-						      <div class="team_image"> 
-						         <img src="<?php echo $root;?>/images/team2.jpg" alt="team"> 
-						       </div>
-						       <div class="team_image_hover">
-						         <h2>Robyn Sullivan - VP </h2>
-						         <p><a href="#" data-reveal-id="team2_modal"><i class="general foundicon-search"></i></a></p>
-						       </div>				      
-						     </div>        
-						   </div>
-						    
-						   <div class="six columns services">  
-						     <div class="services_content">   
-						      <div class="team_image"> 
-						         <img src="<?php echo $root;?>/images/team1.jpg" alt="team"> 
-						       </div>
-						       <div class="team_image_hover">
-						         <h2>Aiko Takahashi - Design </h2>
-						         <p><a href="#" data-reveal-id="team3_modal"><i class="general foundicon-search"></i></a></p>
-						       </div>				      
-						     </div>        
-						   </div>
-		
-						   <div class="six columns services">      
-					       <div class="tile_contents tile-green">
-					         <a href="#" data-reveal-id="map_modal"><i class="general foundicon-location"></i>
-					         <p>Find Us</p></a>
-					       </div>
-						   </div>   
-						    
-		         </div>
-		      </div>    
-		    
-		 	    <div class="six columns">
-			      <div class="row">    
-		      
-		          <div class="six columns services">     
-					      <div class="tile_contents tile-red">
-					        <a href="#"><i class="general foundicon-globe"></i>
-				          <p>Global Presence</p></a>
-				        </div>
-						  </div>			
-						    
-						  <div class="six columns services">     
-						  	 <div class="tile_contents tile-dark-green">
-						       <a href="#"><i class="general foundicon-compass"></i>
-					         <p>Effective Solutions</p></a>
-						     </div>
-						  </div>					    
-						     				    
-						  <div class="six columns services">  
-						    <div class="services_content">   
-						      <div class="team_image"> 
-						        <img src="<?php echo $root;?>/images/team4.jpg" alt="team"> 
-						      </div>
-						      <div class="team_image_hover">
-						        <h2>Frank Douglas - Marketing </h2>
-						        <p><a href="#" data-reveal-id="team4_modal"><i class="general foundicon-search"></i></a></p>
-						      </div>				      
-						    </div>         
-						  </div>
-						    			    
-						  <div class="six columns services">       
-					      <div class="tile_contents tile-orange">
-					        <a href="#" data-reveal-id="contact_modal"><i class="general foundicon-phone"></i>
-					        <p>Contact Us</p></a>
-					      </div>
-					    </div>				    
-						     
-				    </div>
-				  </div> 
-        </div> 			    
-	      <?php } else { print render($page['services']); }?>   
+        <h2>Add a block to the "Front Page Services" block or edit the page--front.tpl template to remove this placeholder content.</h2>
+      <?php } else { print render($page['services']); }?>   
 	    </div>	   
     </div>
       
@@ -202,68 +113,8 @@
   </div>
   <!-- end main wrapper -->    
   
-    <!-- Begin front page modal markup -->    
-       
-    <!-- Google Map Modal -->  
-  	<div id="map_modal" class="reveal-modal large">
-      <div id="google_map">
-        <iframe width="1170" height="370" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=900+Bagby+St+Houston,+TX+77002&amp;sll=33.687,-117.774252&amp;sspn=0.324522,0.682526&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=900+Bagby+St,+Houston,+Harris,+Texas+77002&amp;ll=29.761779,-95.370008&amp;spn=0.003725,0.012864&amp;z=17&amp;iwloc=A&amp;output=embed"></iframe><br />      
-      </div>
-     <a class="close-reveal-modal">&#215;</a>
-    </div>
-    
-    <!-- Team Modal 1 -->
-    <div id="team1_modal" class="reveal-modal medium">
-      <div class="team_bio_modal">
-	      <h1>Steve McQueen - CEO</h1>
-	      <img src="<?php echo $root;?>/images/team3.jpg" alt="team"> 
-	      <h2>Bio</h2>
-	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-	      <h2>Connect</h2>
-	      <a href="http://facebook.com" target="_blank"><i class="social foundicon-facebook"></i></a> <a href="http://twitter.com" target="_blank"><i class="social foundicon-twitter"></i></a> <a href="http://linkedin.com" target="_blank"><i class="social foundicon-linkedin"></i></a>
-	      <a class="close-reveal-modal">&#215;</a>
-      </div>
-    </div>
-    
-    <!-- Team Modal 2 -->
-    <div id="team2_modal" class="reveal-modal medium">
-      <div class="team_bio_modal">
-	      <h1>Robyn Sullivan - Vice President</h1>
-	      <img src="<?php echo $root;?>/images/team2.jpg" alt="team"> 
-	      <h2>Bio</h2>
-	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-	      <h2>Connect</h2>
-	      <a href="http://facebook.com" target="_blank"><i class="social foundicon-facebook"></i></a> <a href="http://twitter.com" target="_blank"><i class="social foundicon-twitter"></i></a> <a href="http://linkedin.com" target="_blank"><i class="social foundicon-linkedin"></i></a>
-	      <a class="close-reveal-modal">&#215;</a>
-      </div>
-    </div>
-    
-    <!-- Team Modal 3 -->
-    <div id="team3_modal" class="reveal-modal medium">
-      <div class="team_bio_modal">
-	      <h1>Aiko Takahashi - Design</h1>
-	      <img src="<?php echo $root;?>/images/team1.jpg" alt="team"> 
-	      <h2>Bio</h2>
-	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-	      <h2>Connect</h2>
-	      <a href="http://facebook.com" target="_blank"><i class="social foundicon-facebook"></i></a> <a href="http://twitter.com" target="_blank"><i class="social foundicon-twitter"></i></a> <a href="http://linkedin.com" target="_blank"><i class="social foundicon-linkedin"></i></a>
-	      <a class="close-reveal-modal">&#215;</a>
-      </div>
-    </div>
-    
-    <!-- Team Modal 3 -->
-    <div id="team4_modal" class="reveal-modal medium">
-      <div class="team_bio_modal">
-	      <h1>Frank Douglas - Marketing</h1>
-	      <img src="<?php echo $root;?>/images/team4.jpg" alt="team"> 
-	      <h2>Bio</h2>
-	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-	      <h2>Connect</h2>
-	      <a href="http://facebook.com" target="_blank"><i class="social foundicon-facebook"></i></a> <a href="http://twitter.com" target="_blank"><i class="social foundicon-twitter"></i></a> <a href="http://linkedin.com" target="_blank"><i class="social foundicon-linkedin"></i></a>
-	      <a class="close-reveal-modal">&#215;</a>
-      </div>
-    </div>
-    
-    <!-- End front page modal markup -->  
+  <!-- Begin front page modal markup -->    
+    <?php print render($page['modal_markup']); ?>       
+  <!-- End front page modal markup -->  
 
 <?php blocks_footer($page); // Call Footer ?>
