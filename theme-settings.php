70957e80870c0f030562514566c75d39210b0d5d
refaktorco <refaktor.co@gmail.com>
2013-06-26 13:28:59 -0700

    -Finally getting back to Blocks 2.0. Doing Twitter fix.

--- theme-settings.php
+++ theme-settings.php
@@ -1,33 +1,12 @@
 <?php
 
 global $slide_number;
-$slide_number = theme_get_setting('slides_number');
+
 
 drupal_add_js(drupal_get_path('theme', 'blocks') .'/js/theme_settings.js'); 
  
 function blocks_form_system_theme_settings_alter(&$form, &$form_state) {
   global $slide_number;
-
- // Default path for image
-  $bg_path = theme_get_setting('bg_path');
-  if (file_uri_scheme($bg_path) == 'public') {
-    $bg_path = file_uri_target($bg_path);
-  }
-  
-  // Default path for back-ground image
-  $background_path = theme_get_setting('background_path');
-  if (file_uri_scheme($background_path) == 'public') {
-    $background_path = file_uri_target($background_path);
-  }  
-  
-  $count = 1;
-  while ($count <= $slide_number){
-    ${'slide_path_' . $count} = theme_get_setting('slide_path_'.$count.'');
-    if (file_uri_scheme(${'slide_path_' . $count}) == 'public') {
-      ${'slide_path_' . $count} = file_uri_target(${'slide_path_' . $count});
-    }  
-  $count++;
-  }
 
   // Container
   $form['options'] = array(
@@ -52,251 +31,7 @@ function blocks_form_system_theme_settings_alter(&$form, &$form_state) {
       '#default_value' => theme_get_setting('breadcrumbs'),
     );
         
-  // Header Options
-  $form['options']['header'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Header'),
-  );
-  
-    // Logo
-    $form['options']['header']['branding'] = array(
-      '#type' => 'fieldset',
-      '#title' => '<div class="plus"></div><h3 class="options_heading">Branding</h3>',
-    );
-    
-      // Logo Toggle
-      $form['options']['header']['branding']['branding_type'] = array(
-        '#type' => 'select',
-        '#title' => 'Branding Type',
-        '#default_value' => theme_get_setting('branding_type'),
-        '#options' => array(
-          'logo' => 'Logo',
-          'text' => 'Text',
-        ),
-      );
-
-      $form['options']['header']['branding']['bg_path'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Path to logo',
-        '#default_value' => $bg_path,
-        '#disabled' => TRUE,
-        '#states' => array(
-          'visible' => array('#edit-branding-type' => array('value' => 'logo')),
-        ), 
-      );
-
-      $form['options']['header']['branding']['bg_upload'] = array(
-        '#type' => 'file',
-        '#title' => 'Upload logo',
-        '#description' => 'Upload a new logo image.',
-        '#states' => array(
-          'visible' => array('#edit-branding-type' => array('value' => 'logo')),
-        ), 
-      );
-	  
-	  //for back-ground image
-	  $form['options']['header']['branding']['background_path'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Path to Background image',
-        '#default_value' => $background_path,
-        '#disabled' => TRUE,
-        '#states' => array(
-          'visible' => array('#edit-branding-type' => array('value' => 'background')),
-        ), 
-      );
-	  
-	  
-	 $form['options']['header']['branding']['background_upload'] = array(
-        '#type' => 'file',
-        '#title' => 'Upload Back-ground image',
-        '#description' => 'Upload a Back-ground image.',
-	
-        '#states' => array(
-          'visible' => array('#edit-branding-type' => array('value' => 'background')),
-        ), 
-      );
-      
-      // Social Icons
-    $form['options']['header']['social'] = array(
-      '#type' => 'fieldset',
-      '#title' => '<div class="plus"></div><h3 class="options_heading">Social Icons</h3>',
-    );
-    
-      // Twitter Icon
-      $form['options']['header']['social']['twitter_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'Twitter Icon',
-        '#default_value' => theme_get_setting('twitter_icon'),
-      );
-      
-      // Twitter Icon URL
-      $form['options']['header']['social']['twitter_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Twitter URL',
-        '#default_value' => theme_get_setting('twitter_url'),
-        '#size' => 10,
-        '#maxlenght' => 10,
-        '#states' => array (
-          'invisible' => array(
-            'input[name="twitter_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-      
-      // Facebook Icon
-      $form['options']['header']['social']['facebook_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'Facebook Icon',
-        '#default_value' => theme_get_setting('facebook_icon'),
-      );
-      
-      // Facebook Icon URL
-      $form['options']['header']['social']['facebook_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Facebook URL',
-        '#default_value' => theme_get_setting('facebook_url'),
-        '#states' => array (
-          'invisible' => array(
-            'input[name="facebook_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-      
-      // Flickr Icon
-      $form['options']['header']['social']['flickr_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'Flickr Icon',
-        '#default_value' => theme_get_setting('flickr_icon'),
-      );
-      
-      // Flickr Icon URL
-      $form['options']['header']['social']['flickr_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Flickr URL',
-        '#default_value' => theme_get_setting('flickr_url'),
-        '#states' => array (
-          'invisible' => array(
-            'input[name="flickr_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-      
-      // Google Plus Icon
-      $form['options']['header']['social']['google_plus_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'Google+ Icon',
-        '#default_value' => theme_get_setting('google_plus_icon'),
-      );
-      
-      // Google Plus URL
-      $form['options']['header']['social']['google_plus_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Google+ URL',
-        '#default_value' => theme_get_setting('google_plus_url'),
-        '#states' => array (
-          'invisible' => array(
-            'input[name="google_plus_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-      
-       // Pinterest Icon
-      $form['options']['header']['social']['pinterest_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'Pinterest Icon',
-        '#default_value' => theme_get_setting('pinterest_icon'),
-      );
-      
-      // Pinterest URL
-      $form['options']['header']['social']['pinterest_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Pinterest URL',
-        '#default_value' => theme_get_setting('pinterest_url'),
-        '#states' => array (
-          'invisible' => array(
-            'input[name="pinterest_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-      
-       // LinkedIn Icon
-      $form['options']['header']['social']['linkedin_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'LinkedIn Icon',
-        '#default_value' => theme_get_setting('linkedin_icon'),
-      );
-      
-      // linkedin URL
-      $form['options']['header']['social']['linkedin_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'LinkedIn URL',
-        '#default_value' => theme_get_setting('linkedin_url'),
-        '#states' => array (
-          'invisible' => array(
-            'input[name="linkedin_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-
-      // Youtube Icon
-      $form['options']['header']['social']['youtube_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'Youtube Icon',
-        '#default_value' => theme_get_setting('youtube_icon'),
-      );
-      
-      // Youtube URL
-      $form['options']['header']['social']['youtube_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Youtube URL',
-        '#default_value' => theme_get_setting('youtube_url'),
-        '#states' => array (
-          'invisible' => array(
-            'input[name="youtube_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-      
-      // Vimeo Icon
-      $form['options']['header']['social']['vimeo_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'Vimeo Icon',
-        '#default_value' => theme_get_setting('vimeo_icon'),
-      );
-      
-      // Youtube URL
-      $form['options']['header']['social']['vimeo_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Vimeo URL',
-        '#default_value' => theme_get_setting('vimeo_url'),
-        '#states' => array (
-          'invisible' => array(
-            'input[name="vimeo_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-
-      // RSS Icon
-      $form['options']['header']['social']['rss_icon'] = array(
-        '#type' => 'checkbox',
-        '#title' => 'RSS Icon',
-        '#default_value' => theme_get_setting('rss_icon'),
-      );
-      
-      // Twitter URL
-      $form['options']['header']['social']['rss_url'] = array(
-        '#type' => 'textfield',
-        '#title' => 'RSS URL',
-        '#default_value' => theme_get_setting('rss_url'),
-        '#states' => array (
-          'invisible' => array(
-            'input[name="rss_icon"]' => array('checked' => FALSE)
-          )
-        )
-      );
-
-	
-    // Layout
+      // Layout
   $form['options']['layout'] = array(
     '#type' => 'fieldset',
     '#title' => 'Layout',
@@ -505,59 +240,64 @@ function blocks_form_system_theme_settings_alter(&$form, &$form_state) {
         '#title' => 'h6 Size',
         '#default_value' => theme_get_setting('h6'),
       );
+      
+    // Twitter
+  $form['options']['twitter'] = array(
+    '#type' => 'fieldset',
+    '#title' => 'Twitter',
+  );    
+  
+     // Twitter App Consumer Key
+    $form['options']['twitter']['twitter_app_consumer_key'] =array(
+      '#type' => 'textfield',
+      '#title' => 'Twitter App Consumer Key',
+      '#default_value' => theme_get_setting('twitter_app_consumer_key'),
+      '#states' => array (
+        'invisible' => array(
+          'input[name="enable_twitter_feed"]' => array('checked' => FALSE)
+        )
+      )
+    );
+    
+    // Twitter App Consumer Secret
+    $form['options']['twitter']['twitter_app_consumer_secret'] =array(
+      '#type' => 'textfield',
+      '#title' => 'Twitter App Consumer Secret',
+      '#default_value' => theme_get_setting('twitter_app_consumer_secret'),
+      '#states' => array (
+        'invisible' => array(
+          'input[name="enable_twitter_feed"]' => array('checked' => FALSE)
+        )
+      )
+    );
+    
+    // Twitter App Access Token
+    $form['options']['twitter']['twitter_app_access_token'] =array(
+      '#type' => 'textfield',
+      '#title' => 'Twitter App Access Token',
+      '#default_value' => theme_get_setting('twitter_app_access_token'),
+      '#states' => array (
+        'invisible' => array(
+          'input[name="enable_twitter_feed"]' => array('checked' => FALSE)
+        )
+      )
+    );
+    
+    // Twitter App Access Token Secret
+    $form['options']['twitter']['twitter_app_access_secret'] =array(
+      '#type' => 'textfield',
+      '#title' => 'Twitter App Access Token Secret',
+      '#default_value' => theme_get_setting('twitter_app_access_secret'),
+      '#states' => array (
+        'invisible' => array(
+          'input[name="enable_twitter_feed"]' => array('checked' => FALSE)
+        )
+      )
+    );
+
               
-  // Submit Button
-  $form['#submit'][] = 'blocks_settings_submit';
-  $form['#submit'][] = 'blocks_background_settings_submit';
-  
-}
-
-function blocks_settings_submit($form, &$form_state) {
-  // Get the previous value
-  $previous = 'public://' . $form['options']['header']['branding']['bg_path']['#default_value'] ;
-  
-  $file = file_save_upload('bg_upload');
-  if ($file) {
-    $parts = pathinfo($file->filename);
-    $destination = 'public://' . $parts['basename'];
-    $file->status = FILE_STATUS_PERMANENT;
     
-    if(file_copy($file, $destination, FILE_EXISTS_REPLACE)) {
-      $_POST['bg_path'] = $form_state['values']['bg_path'] = $destination;
-      if ($destination != $previous) {
-        return;
-      }
-    }
-  } else {
-    // Avoid error when the form is submitted without specifying a new image
-    $_POST['bg_path'] = $form_state['values']['bg_path'] = $previous;
-  }
-  
 }
 
-// for background image//ntf
-function blocks_background_settings_submit($form, &$form_state) {
-
-  // Get the previous value
-  $previous = 'public://' . $form['options']['header']['branding']['background_path']['#default_value'] ;
-  
-  $file = file_save_upload('background_upload');
-  if ($file) {
-    $parts = pathinfo($file->filename);
-    $destination = 'public://' . $parts['basename'];
-    $file->status = FILE_STATUS_PERMANENT;
-    
-    if(file_copy($file, $destination, FILE_EXISTS_REPLACE)) {
-      $_POST['background_path'] = $form_state['values']['background_path'] = $destination;
-      if ($destination != $previous) {
-        return;
-      }
-    }
-  } else {
-    // Avoid error when the form is submitted without specifying a new image
-    $_POST['background_path'] = $form_state['values']['background_path'] = $previous;
-  }
-  
-}
 
 ?>
